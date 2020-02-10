81ff714f38e28112e9107b782e97e6d71b17456f
Dongxia Wu <379009336ddx@gmail.com>
2020-02-10 02:35:11 -0600

    Button demo

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,10 +1,32 @@
 package com.example.lab2;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction (View view){
+//        Log.i( "Info",  "Button pressed");
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String str = myTextField.getText().toString();
+        gotoActivity2(str);
+
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+
+    }
+
+    public void gotoActivity2(String s){
+        Intent intent = new Intent(this,Main2Activity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
